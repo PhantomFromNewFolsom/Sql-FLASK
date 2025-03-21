8d5e39c85e9bed61472caab475a075635640d3fd
PhantomFromNewFolsom <podzizeyal@gmail.com>
2025-03-21 15:00:04 +0500

    Решение пятой классной задачи [WEB. REST-API. Понятие. Делаем простое Rest-api]

--- test.py
+++ test.py
@@ -4,14 +4,18 @@ print(get('http://localhost:8080/api/jobs').json())
 
 print(get('http://localhost:8080/api/jobs/2').json())
 
+#  работы с таким id не существует
 print(get('http://localhost:8080/api/jobs/20234234903490').json())
 
+#  вместо номера посылаем строку
 print(get('http://localhost:8080/api/jobs/BlaBlaBlaBleBleBleBluBluBlu').json())
 
+#  пустой запрос, ничего отправляем
 print(post('http://localhost:8080/api/jobs', json={}).json())
 
+#  неполный запрос, не хватает полей
 print(post('http://localhost:8080/api/jobs',
-           json={'title': 'Заголовок'}).json())
+           json={'job': 'РАБОТААА'}).json())
 
 print(post('http://localhost:8080/api/jobs',
            json={'team_leader': 2,
@@ -21,3 +25,6 @@ print(post('http://localhost:8080/api/jobs',
                  'start_date': [2025, 3, 18],
                  'end_date': [2025, 3, 18],
                  'is_finished': False}).json())
+
+#  проверяем, добавилась ли работа
+print(get('http://localhost:8080/api/jobs').json())
