9b80d3d737f66b90d7ffdc6abe667e1a02a9a003
PhantomFromNewFolsom <podzizeyal@gmail.com>
2025-03-21 14:50:07 +0500

    Решение четвёртой классной задачи [WEB. REST-API. Понятие. Делаем простое Rest-api]

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -1,5 +1,7 @@
+import datetime
+
 import flask
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 
 from . import db_session
 from .jobs import Job
@@ -28,10 +30,10 @@ def get_jobs():
 @blueprint.route('/api/jobs/<jobs_id>', methods=['GET'])
 def get_one_jobs(jobs_id):
     db_sess = db_session.create_session()
-    print(type(jobs_id) is not int, type(jobs_id))
-    print(jobs_id)
-    if type(jobs_id) is not int:
-        return make_response(jsonify({'error': 'Bad request'}), 400)
+    try:
+        jobs_id = int(jobs_id)
+    except Exception:
+        return make_response(jsonify({'error': 'Bad request'}), 404)
     jobs = db_sess.query(Job).get(jobs_id)
     if not jobs:
         return make_response(jsonify({'error': 'Not found'}), 404)
@@ -41,3 +43,29 @@ def get_one_jobs(jobs_id):
                                        'start_date', 'end_date', 'is_finished'))
         }
     )
+
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_jobs():
+    if not request.json:
+        return make_response(jsonify({'error': 'Empty request'}), 400)
+    elif not all(key in request.json for key in
+                 ['team_leader', 'job', 'work_size', 'collaborators', 'start_date', 'end_date', 'is_finished']):
+        return make_response(jsonify({'error': 'Bad request'}), 400)
+    db_sess = db_session.create_session()
+    start_date = datetime.datetime(request.json['start_date'][0], request.json['start_date'][1],
+                                   request.json['start_date'][2])
+    end_date = datetime.datetime(request.json['end_date'][0], request.json['end_date'][1],
+                                 request.json['end_date'][2])
+    jobs = Job(
+        team_leader=request.json['team_leader'],
+        job=request.json['job'],
+        work_size=request.json['work_size'],
+        collaborators=request.json['collaborators'],
+        start_date=start_date,
+        end_date=end_date,
+        is_finished=request.json['is_finished']
+    )
+    db_sess.add(jobs)
+    db_sess.commit()
+    return jsonify({'id': jobs.id})
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from requests import get
+from requests import get, post
 
 print(get('http://localhost:8080/api/jobs').json())
 
@@ -8,3 +8,16 @@ print(get('http://localhost:8080/api/jobs/20234234903490').json())
 
 print(get('http://localhost:8080/api/jobs/BlaBlaBlaBleBleBleBluBluBlu').json())
 
+print(post('http://localhost:8080/api/jobs', json={}).json())
+
+print(post('http://localhost:8080/api/jobs',
+           json={'title': 'Заголовок'}).json())
+
+print(post('http://localhost:8080/api/jobs',
+           json={'team_leader': 2,
+                 'job': 'Важная работа',
+                 'work_size': 10,
+                 'collaborators': '3, 4, 5',
+                 'start_date': [2025, 3, 18],
+                 'end_date': [2025, 3, 18],
+                 'is_finished': False}).json())
